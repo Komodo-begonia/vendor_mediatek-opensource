1d4ab0404f48a6e8b049fce9ea89028899742108
Zinadin Zidan <zidan44@pixelexperience.org>
2021-06-28 17:55:37 +0600

    vendor: mtk: frameworks: Import mtk openmax headers
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Ida8cdbe0328dfaeefae50740743b42e887376af2

---
+++ frameworks/Android.mk
@@ -0,0 +1,4 @@
+FW_PATH := $(call my-dir)
+
+# MTK Frameworks
+include $(call all-makefiles-under, $(FW_PATH))
--- mtk-builds.mk
+++ mtk-builds.mk
@@ -10,6 +10,9 @@ $(call inherit-product, vendor/mediatek/opensource/custom/mtk-custom.mk)
 # Call External (MTK)
 $(call inherit-product, vendor/mediatek/opensource/external/mtk-external.mk)
 
+# Call Frameworks (MTK)
+$(call inherit-product, vendor/mediatek/opensource/frameworks/mtk-frameworks.mk)
+
 # Call Hardware (MTK)
 $(call inherit-product, vendor/mediatek/opensource/hardware/mtk-hardware.mk)
 
