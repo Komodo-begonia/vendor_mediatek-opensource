49af1720faac5b5618facebc72cc0669726aaa93
Zinadin Zidan <zidan.roking@gmail.com>
2021-08-12 02:41:33 +0600

    vendor: mtk: keymaster: Add vintf fragments for keymaster hals
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I092aa9dca829074c7edbf5ce1ae4535828787912

--- hardware/libkmsetkey/1.1/Android.mk
+++ hardware/libkmsetkey/1.1/Android.mk
@@ -22,6 +22,7 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := vendor.mediatek.hardware.keymaster_attestation@1.1-service
 LOCAL_INIT_RC := vendor.mediatek.hardware.keymaster_attestation@1.1-service.rc
+LOCAL_VINTF_FRAGMENTS := manifest.keymaster_attestation.xml
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_OWNER := mtk
