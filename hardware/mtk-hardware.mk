564396b9ab15857adeb0841ad34b95be1bd9bf48
Zinadin Zidan <zidan.roking@gmail.com>
2021-08-24 19:16:03 +0600

    vendor: mtk: Drop mtk oss trustzone keymaster
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I806dae3e4eff13f1272f14d296d0fccb3f2f1843

--- hardware/mtk-hardware.mk
+++ hardware/mtk-hardware.mk
@@ -11,8 +11,6 @@ $(call inherit-product, vendor/mediatek/opensource/hardware/connectivity/connect
 PRODUCT_PACKAGES += \
     fstb.cfg \
     libpowerhal \
-    libkmsetkey \
-    kmsetkey_ca \
     libccci_util \
     libfpspolicy \
     power.default \
@@ -35,6 +33,4 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service-mediatek \
     vendor.mediatek.hardware.dfps@1.0-service \
     vendor.mediatek.hardware.mtkpower@1.0-impl \
-    vendor.mediatek.hardware.mtkpower@1.0-service \
-    vendor.mediatek.hardware.keymaster_attestation@1.1-impl \
-    vendor.mediatek.hardware.keymaster_attestation@1.1-service
+    vendor.mediatek.hardware.mtkpower@1.0-service
